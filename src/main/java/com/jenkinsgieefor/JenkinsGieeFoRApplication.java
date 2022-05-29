d13dd0aaf6d432b745cf707bee22a316a3b61b22
GieeFoR <kamilooo1998@onet.pl>
2022-05-29 16:01:01 +0200

    Lab9

---
+++ src/main/java/com/jenkinsgieefor/JenkinsGieeFoRApplication.java
@@ -0,0 +1,33 @@
+package com.jenkinsgieefor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@SpringBootApplication
+public class JenkinsGieeFoRApplication {
+
+    @Autowired
+    Environment env;
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsGieeFoRApplication.class, args);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("driverClassName")));
+        dataSource.setUrl(env.getProperty("url"));
+        dataSource.setUsername(env.getProperty("user"));
+        dataSource.setPassword(env.getProperty("password"));
+        return dataSource;
+    }
+
+}
